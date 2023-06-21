67dd795fbb985317835e06a260b65e97ce4b444c
Pozitronik <antikillerxxl@gmail.com>
2023-06-21 12:00:25 +0400

    Make state into a class

--- roop/state.py
+++ roop/state.py
@@ -1,53 +1,52 @@
 import os
-from typing import Union
 
 from roop.capturer import get_video_frame_total
+from roop.parameters import Parameters
 from roop.utilities import get_temp_directory_path
 
-PROCESSED_PREFIX = '_'
 
+class State:
+    PROCESSED_PREFIX = '_'
 
-#  Checks if all frames in the target file temp folder are processed
-def is_done(target_path: str) -> bool:
-    return (processed_frames_count(target_path) > 0) and (0 == unprocessed_frames_count(target_path))
+    target_path: str
 
+    def __init__(self, params: Parameters):
+        self.target_path = params.target_path
 
-#  Checks if the temp directory with frames is existed and all frames are extracted (or some already processed) for target
-def is_resumable(target_path: str) -> bool:
-    return (processed_frames_count(target_path) > 0) or unprocessed_frames_count(target_path) == get_video_frame_total(target_path)
+    #  Checks if all frames in the target file temp folder are processed
+    def is_done(self) -> bool:
+        return (self.processed_frames_count() > 0) and (0 == self.unprocessed_frames_count())
 
+    #  Checks if the temp directory with frames is existed and all frames are extracted (or some already processed) for target
+    def is_resumable(self) -> bool:
+        return (self.processed_frames_count() > 0) or self.unprocessed_frames_count() == get_video_frame_total(self.target_path)
 
-#  Checks if the temp directory with frames is completely processed (and can be deleted)
-def is_finished(target_path: Union[None, str]) -> bool:
-    return target_path is not None \
-        and is_done(target_path) and processed_frames_count(target_path) == get_video_frame_total(target_path)
+    #  Checks if the temp directory with frames is completely processed (and can be deleted)
+    def is_finished(self) -> bool:
+        return self.is_done() and self.processed_frames_count() == get_video_frame_total(self.target_path)
 
+    #  Returns count of already processed frames for this target path (0, if none). Once called, stores value in state.processed_frames_cnt global variable
+    def processed_frames_count(self) -> int:
+        directory = get_temp_directory_path(self.target_path)
+        if not os.path.exists(directory): return 0
+        return len(
+            [os.path.join(directory, file) for file in os.listdir(directory) if
+             file.startswith(self.PROCESSED_PREFIX) and file.endswith(".png")])
 
-#  Returns count of already processed frames for this target path (0, if none). Once called, stores value in state.processed_frames_cnt global variable
-def processed_frames_count(target_path: str) -> int:
-    directory = get_temp_directory_path(target_path)
-    if not os.path.exists(directory): return 0
-    return len(
-        [os.path.join(directory, file) for file in os.listdir(directory) if
-         file.startswith(PROCESSED_PREFIX) and file.endswith(".png")])
+    #  Returns count of still unprocessed frames for this target path (0, if none).
+    def unprocessed_frames_count(self) -> int:
+        directory = get_temp_directory_path(self.target_path)
+        if not os.path.exists(directory): return 0
+        return len([os.path.join(directory, file) for file in os.listdir(directory) if
+                    self.PROCESSED_PREFIX not in file and file.endswith(".png")])
 
+    #  Returns count all frames for this target path, processed and unprocessed (0, if none).
+    def total_frames_count(self) -> int:
+        directory = get_temp_directory_path(self.target_path)
+        if not os.path.exists(directory): return 0
+        return len([os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".png")])
 
-#  Returns count of still unprocessed frames for this target path (0, if none).
-def unprocessed_frames_count(target_path: str) -> int:
-    directory = get_temp_directory_path(target_path)
-    if not os.path.exists(directory): return 0
-    return len([os.path.join(directory, file) for file in os.listdir(directory) if
-                PROCESSED_PREFIX not in file and file.endswith(".png")])
-
-
-#  Returns count all frames for this target path, processed and unprocessed (0, if none).
-def total_frames_count(target_path: str) -> int:
-    directory = get_temp_directory_path(target_path)
-    if not os.path.exists(directory): return 0
-    return len([os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".png")])
-
-
-#  Returns a processed file name for an unprocessed frame file name
-def get_frame_processed_name(unprocessed_frame_name: str) -> str:
-    directory, filename = os.path.split(unprocessed_frame_name)
-    return str(os.path.join(directory, PROCESSED_PREFIX + filename))
+    #  Returns a processed file name for an unprocessed frame file name
+    def get_frame_processed_name(self, unprocessed_frame_name: str) -> str:
+        directory, filename = os.path.split(unprocessed_frame_name)
+        return str(os.path.join(directory, self.PROCESSED_PREFIX + filename))
