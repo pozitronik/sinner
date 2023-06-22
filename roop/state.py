523b327b88a69ed6475ae0703e563e2a68297752
Pozitronik <antikillerxxl@gmail.com>
2023-06-22 13:33:07 +0400

    State now handles single frame targets (i.e. simple images)

--- roop/state.py
+++ roop/state.py
@@ -4,31 +4,34 @@ from typing import List
 
 from roop.capturer import get_video_frame_total
 from roop.parameters import Parameters
-from roop.utilities import get_temp_directory_path
+from roop.utilities import get_temp_directory_path, is_video
 
 
 class State:
     PROCESSED_PREFIX = '_'
 
     target_path: str
+    is_multi_frame: bool  # for single frame changes (i.e. picture to picture) the state is always persistent
 
     def __init__(self, params: Parameters):
         self.target_path = params.target_path
+        self.is_multi_frame = is_video(self.target_path)
 
     #  Checks if all frames in the target file temp folder are processed
     def is_done(self) -> bool:
-        return (self.processed_frames_count() > 0) and (0 == self.unprocessed_frames_count())
+        return self.is_multi_frame and (self.processed_frames_count() > 0) and (0 == self.unprocessed_frames_count())
 
     #  Checks if the temp directory with frames is existed and all frames are extracted (or some already processed) for target
     def is_resumable(self) -> bool:
-        return (self.processed_frames_count() > 0) or self.unprocessed_frames_count() == get_video_frame_total(self.target_path)
+        return self.is_multi_frame and (self.processed_frames_count() > 0) or self.unprocessed_frames_count() == get_video_frame_total(self.target_path)
 
     #  Checks if the temp directory with frames is completely processed (and can be deleted)
     def is_finished(self) -> bool:
-        return self.is_done() and self.processed_frames_count() == get_video_frame_total(self.target_path)
+        return self.is_multi_frame and self.is_done() and self.processed_frames_count() == get_video_frame_total(self.target_path)
 
     #  Returns count of already processed frames for this target path (0, if none). Once called, stores value in state.processed_frames_cnt global variable
     def processed_frames_count(self) -> int:
+        if not self.is_multi_frame: return 0;
         directory = get_temp_directory_path(self.target_path)
         if not os.path.exists(directory): return 0
         return len(
@@ -37,6 +40,7 @@ class State:
 
     #  Returns count of still unprocessed frames for this target path (0, if none).
     def unprocessed_frames_count(self) -> int:
+        if not self.is_multi_frame: return 1;
         directory = get_temp_directory_path(self.target_path)
         if not os.path.exists(directory): return 0
         return len([os.path.join(directory, file) for file in os.listdir(directory) if
@@ -44,6 +48,7 @@ class State:
 
     #  Returns count all frames for this target path, processed and unprocessed (0, if none).
     def total_frames_count(self) -> int:
+        if not self.is_multi_frame: return 1;
         directory = get_temp_directory_path(self.target_path)
         if not os.path.exists(directory): return 0
         return len([os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".png")])
@@ -56,4 +61,6 @@ class State:
     #  Returns all unprocessed frames
     def unprocessed_frames(self) -> List[str]:
         temp_directory_path = get_temp_directory_path(self.target_path)
+        if not self.is_multi_frame:
+            return [os.path.join(glob.escape(temp_directory_path), os.path.basename(self.target_path))];
         return [file for file in glob.glob(os.path.join(glob.escape(temp_directory_path), '*.png')) if not os.path.basename(file).startswith(self.PROCESSED_PREFIX)]
