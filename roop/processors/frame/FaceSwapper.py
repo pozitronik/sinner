1d581fd477f95fcfb2208ba565f7ff13d9863524
Pozitronik <antikillerxxl@gmail.com>
2023-06-22 16:57:55 +0400

    new property implemented

--- roop/processors/frame/FaceSwapper.py
+++ roop/processors/frame/FaceSwapper.py
@@ -72,7 +72,7 @@ class FaceSwapper(BaseFrameProcessor):
     def process(self):
         update_status(f'Temp resources for this target already exists with {self.state.processed_frames_count()} frames processed, continue processing...')
         progress_bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
-        total = self.state.total_frames_count()
+        total = self.state.frames_count
         with tqdm(total=total, desc='Processing', unit='frame', dynamic_ncols=True, bar_format=progress_bar_format, initial=self.state.processed_frames_count()) as progress:
             progress.set_postfix({'execution_providers': self.execution_providers, 'threads': self.execution_threads, 'memory': self.max_memory})
             self.multi_process_frame(self.source, self.state.unprocessed_frames(), self.process_frames, progress)
