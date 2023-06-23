3fc054dd7b4f566d60cb92d9a2d74c9ec4f8307f
Pozitronik <antikillerxxl@gmail.com>
2023-06-23 22:16:27 +0400

    Increase required python version

--- run.py
+++ run.py
@@ -51,8 +51,8 @@ def get_frame_processor(state: State) -> BaseFrameProcessor:  # temporary, will 
 
 
 if __name__ == '__main__':
-    if sys.version_info < (3, 9):
-        raise Exception('Python version is not supported - please upgrade to 3.9 or higher.')
+    if sys.version_info < (3, 10):
+        raise Exception('Python version is not supported - please upgrade to 3.10 or higher.')
     signal.signal(signal.SIGINT, lambda signal_number, frame: destroy())
 
     params = Parameters()
