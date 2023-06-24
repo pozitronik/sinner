704949d03974cb7c602adb4ffd3750f4ac41b5c6
Pozitronik <antikillerxxl@gmail.com>
2023-06-24 12:40:14 +0400

    types clarification

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -1,16 +1,16 @@
 from typing import List
-import insightface
+from insightface.app import FaceAnalysis
 
 from roop.typing import Frame, Face
 
 
 class FaceAnalyser:
-    _face_analyser = None
+    _face_analyser: FaceAnalysis
     _execution_providers: List[str]
 
     def __init__(self, execution_providers: List[str]):
         self._execution_providers = execution_providers
-        self._face_analyser = insightface.app.FaceAnalysis(name='buffalo_l', providers=self._execution_providers)
+        self._face_analyser = FaceAnalysis(name='buffalo_l', providers=self._execution_providers)
         self._face_analyser.prepare(ctx_id=0, det_size=(640, 640))
 
     def get_one_face(self, frame: Frame) -> None | Face:
