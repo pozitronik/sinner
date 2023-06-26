e8308c4a913ec72be1b5abc7b759556fa987556e
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 20:46:36 +0400

    source_face fixed

--- roop/processors/frame/FaceSwapper.py
+++ roop/processors/frame/FaceSwapper.py
@@ -14,24 +14,25 @@ from roop.utilities import resolve_relative_path, conditional_download, update_s
 class FaceSwapper(BaseFrameProcessor):
     many_faces: bool = False
     state: State
+    source_face: Face
 
     _face_analyser: FaceAnalyser
     _face_swapper: FaceSwapperType
-    _source_face: None | Face
 
     THREAD_LOCK = threading.Lock()
 
-    def __init__(self, execution_providers: List[str], execution_threads: int, max_memory: int, many_faces: bool, state: State):
+    def __init__(self, execution_providers: List[str], execution_threads: int, max_memory: int, many_faces: bool, source_face: Frame, state: State):
         download_directory_path = resolve_relative_path('../models')
         conditional_download(download_directory_path, ['https://huggingface.co/henryruhs/roop/resolve/main/inswapper_128.onnx'])
         self._face_analyser = FaceAnalyser(self.execution_providers)
         super().__init__(execution_providers=execution_providers, execution_threads=execution_threads, max_memory=max_memory)
         self.many_faces = many_faces
         self.state = state
+        self.source_face = self._face_analyser.get_one_face(source_face)
         self._face_swapper = insightface.model_zoo.get_model(resolve_relative_path('../models/inswapper_128.onnx'), providers=self.execution_providers)
 
     def swap_face(self, target_face: Face, temp_frame: Frame) -> Frame:
-        return self._face_swapper.get(temp_frame, target_face, self._source_face, paste_back=True)
+        return self._face_swapper.get(temp_frame, target_face, self.source_face, paste_back=True)
 
     def process_frame(self, temp_frame: Frame) -> Frame:
         if self.many_faces:
--- roop/prototypes.py
+++ roop/prototypes.py
@@ -5,6 +5,7 @@ from roop.parameters import Parameters
 from roop.processors.frame.BaseFrameProcessor import BaseFrameProcessor
 from roop.processors.frame.FaceSwapper import FaceSwapper
 from roop.state import State
+from roop.utilities import read_image
 
 
 def get_video_handler(target_path: str, handler_name: str = 'ffmpeg') -> BaseFramesHandler:  # temporary, will be replaced with a factory
@@ -14,4 +15,5 @@ def get_video_handler(target_path: str, handler_name: str = 'ffmpeg') -> BaseFra
 
 
 def get_frame_processor(params: Parameters, state: State) -> BaseFrameProcessor:  # temporary, will be replaced with a factory
-    return FaceSwapper(execution_providers=params.execution_providers, execution_threads=params.execution_threads, max_memory=params.max_memory, many_faces=params.many_faces, state=state)
+    return FaceSwapper(execution_providers=params.execution_providers, execution_threads=params.execution_threads, max_memory=params.max_memory, many_faces=params.many_faces, source_face=read_image(params.source_path),
+                       state=state)
