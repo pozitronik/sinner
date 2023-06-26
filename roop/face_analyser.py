46106a762b7867818c63182f5051f3527504eacb
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 21:35:22 +0400

    Add thread lock to face analyser

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -1,3 +1,4 @@
+import threading
 from typing import List
 from insightface.app import FaceAnalysis
 
@@ -5,13 +6,16 @@ from roop.typing import Frame, Face
 
 
 class FaceAnalyser:
+    THREAD_LOCK = threading.Lock()
+
     _face_analyser: FaceAnalysis
     _execution_providers: List[str]
 
     def __init__(self, execution_providers: List[str]):
         self._execution_providers = execution_providers
-        self._face_analyser = FaceAnalysis(name='buffalo_l', providers=self._execution_providers)
-        self._face_analyser.prepare(ctx_id=0, det_size=(640, 640))
+        with self.THREAD_LOCK:
+            self._face_analyser = FaceAnalysis(name='buffalo_l', providers=self._execution_providers)
+            self._face_analyser.prepare(ctx_id=0, det_size=(640, 640))
 
     def get_one_face(self, frame: Frame) -> None | Face:
         face = self._face_analyser.get(frame)
