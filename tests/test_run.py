da6497c2c6d932555066e949cf2f0e8be1b4592b
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 13:20:02 +0400

    test frames

--- tests/test_run.py
+++ tests/test_run.py
@@ -16,6 +16,7 @@ result_jpg: str = resolve_relative_path('data/results/result.jpg', __file__)
 result_mp4: str = resolve_relative_path('data/results/result.mp4', __file__)
 results_dir: str = resolve_relative_path('data/results/', __file__)
 tmp_dir: str = resolve_relative_path('data/targets/temp', __file__)
+state_frames_dir: str = resolve_relative_path('data/frames', __file__)
 
 
 def setup():
@@ -49,6 +50,41 @@ def test_image_to_video_ffmpeg():
     ))
     state = State(params)
     state.create()
+    limit_resources(params.max_memory)
+    core = Core(params=params, state=state, frames_handler=get_video_handler(params.target_path, params.frame_handler), frame_processor=get_frame_processor(params, state))
+    core.run()
+
+    frames_count = len([file for file in os.listdir(resolve_relative_path('data/targets/temp/target.mp4/source.jpg', __file__))])
+    assert frames_count == 62
+    assert is_video(result_mp4)
+    test_video_handler = get_video_handler(result_mp4, params.frame_handler)
+    assert test_video_handler.fc == 62
+    assert test_video_handler.fps == 30
+
+
+# test processing continuation
+def test_image_to_video_ffmpeg_state():
+    params = Parameters(Namespace(
+        max_memory=4,
+        execution_provider='cuda',
+        execution_threads=4,
+        keep_audio=True,
+        keep_frames=True,
+        frame_processor='FaceSwapper',
+        frame_handler='ffmpeg',
+        fps=None,
+        many_faces=False,
+        source_path=source_jpg,
+        target_path=target_mp4,
+        output_path=result_mp4,
+    ))
+    state = State(params)
+    state.create()
+    assert [] == os.listdir(state.out_dir)  # check if out directory is empty
+    for filename in [f'{"%04d" % i}.png' for i in range(1, 31)]:  # copy files from 0001.png to 0030.png to out dir
+        shutil.copy(os.path.join(state_frames_dir, filename), os.path.join(state.out_dir, filename))
+    assert 30 == len(os.listdir(state.out_dir))
+    assert 30 == state.processed_frames_count()
     limit_resources(params.max_memory)
     core = Core(params=params, state=state, frames_handler=get_video_handler(params.target_path, params.frame_handler), frame_processor=get_frame_processor(params, state))
     core.run()
