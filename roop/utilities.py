dd2957d85f4a732f2d87cb79440031e2b792ecbc
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 12:08:10 +0400

    fixes video files recognition

--- roop/utilities.py
+++ roop/utilities.py
@@ -99,7 +99,7 @@ def is_image(image_path: str | None) -> bool:
 def is_video(video_path: str) -> bool:
     if video_path and os.path.isfile(video_path):
         mimetype, _ = mimetypes.guess_type(video_path)
-        return bool(mimetype and mimetype.startswith('frames/'))
+        return bool(mimetype and (mimetype.startswith('frames/') or mimetype.startswith('video/')))
     return False
 
 
