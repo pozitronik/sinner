5f8de2fe9dcb7e62bc53fa731e4444c7646f610a
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 13:16:09 +0400

    Remove old code

--- roop/processors/frame/FaceSwapper.py
+++ roop/processors/frame/FaceSwapper.py
@@ -73,6 +73,4 @@ class FaceSwapper(BaseFrameProcessor):
         total = self.state.frames_count
         with tqdm(total=total, desc='Processing', unit='frame', dynamic_ncols=True, bar_format=progress_bar_format, initial=self.state.processed_frames_count()) as progress:
             progress.set_postfix({'execution_providers': self.execution_providers, 'threads': self.execution_threads, 'memory': self.max_memory})
-            if hasattr(frames_provider, 'current_frame_index'):
-                frames_provider.current_frame_index = self.state.processed_frames_count()
             self.multi_process_frame(frames_provider, self.process_frames, progress)
