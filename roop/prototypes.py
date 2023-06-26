5ee10f3fb8d23abc54352cf2110ad5e24ae26a11
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 22:07:23 +0400

    code formatting

--- roop/prototypes.py
+++ roop/prototypes.py
@@ -15,5 +15,11 @@ def get_video_handler(target_path: str, handler_name: str = 'ffmpeg') -> BaseFra
 
 
 def get_frame_processor(params: Parameters, state: State) -> BaseFrameProcessor:  # temporary, will be replaced with a factory
-    return FaceSwapper(execution_providers=params.execution_providers, execution_threads=params.execution_threads, max_memory=params.max_memory, many_faces=params.many_faces, source_face=read_image(params.source_path),
-                       state=state)
+    return FaceSwapper(
+        execution_providers=params.execution_providers,
+        execution_threads=params.execution_threads,
+        max_memory=params.max_memory,
+        many_faces=params.many_faces,
+        source_face=read_image(params.source_path),
+        state=state
+    )
