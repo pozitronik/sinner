895a2e88e502f84f6050b8a9ac887188d267ccce
Pozitronik <antikillerxxl@gmail.com>
2023-06-26 13:21:55 +0400

    continuation tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -63,7 +63,7 @@ def test_image_to_video_ffmpeg():
 
 
 # test processing continuation
-def test_image_to_video_ffmpeg_state():
+def test_image_to_video_ffmpeg_continue():
     params = Parameters(Namespace(
         max_memory=4,
         execution_provider='cuda',
@@ -114,6 +114,40 @@ def test_image_to_video_cv2():
     ))
     state = State(params)
     state.create()
+    limit_resources(params.max_memory)
+    core = Core(params=params, state=state, frames_handler=get_video_handler(params.target_path, params.frame_handler), frame_processor=get_frame_processor(params, state))
+    core.run()
+
+    frames_count = len([file for file in os.listdir(resolve_relative_path('data/targets/temp/target.mp4/source.jpg', __file__))])
+    assert frames_count == 62
+    assert is_video(result_mp4)
+    test_video_handler = get_video_handler(result_mp4, params.frame_handler)
+    assert test_video_handler.fc == 62
+    assert test_video_handler.fps == 30
+
+
+def test_image_to_video_cv2_continue():
+    params = Parameters(Namespace(
+        max_memory=4,
+        execution_provider='cuda',
+        execution_threads=4,
+        keep_audio=True,
+        keep_frames=True,
+        frame_processor='FaceSwapper',
+        frame_handler='cv2',
+        fps=None,
+        many_faces=False,
+        source_path=source_jpg,
+        target_path=target_mp4,
+        output_path=result_mp4,
+    ))
+    state = State(params)
+    state.create()
+    assert [] == os.listdir(state.out_dir)  # check if out directory is empty
+    for filename in [f'{"%04d" % i}.png' for i in range(1, 31)]:  # copy files from 0001.png to 0030.png to out dir
+        shutil.copy(os.path.join(state_frames_dir, filename), os.path.join(state.out_dir, filename))
+    assert 30 == len(os.listdir(state.out_dir))
+    assert 30 == state.processed_frames_count()
     limit_resources(params.max_memory)
     core = Core(params=params, state=state, frames_handler=get_video_handler(params.target_path, params.frame_handler), frame_processor=get_frame_processor(params, state))
     core.run()
