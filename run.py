978e77bd0d38904b45d0a437f26a3de0df9e21a7
Pozitronik <antikillerxxl@gmail.com>
2023-06-27 17:22:45 +0400

    refactoring

--- run.py
+++ run.py
@@ -9,18 +9,10 @@ from roop.processors.frame.BaseFrameProcessor import BaseFrameProcessor
 from roop.state import State
 from roop.utilities import limit_resources
 
-
-def destroy() -> None:
-    # todo
-    # if state.is_finished():
-    #     clean_temp(params.target_path, params.keep_frames)
-    exit()
-
-
 if __name__ == '__main__':
     if sys.version_info < (3, 10):
         raise Exception('Python version is not supported - please upgrade to 3.10 or higher.')
-    signal.signal(signal.SIGINT, lambda signal_number, frame: destroy())
+    signal.signal(signal.SIGINT, lambda signal_number, frame: quit())
 
     params = Parameters()
     state = State(source_path=params.source_path, target_path=params.target_path, output_path=params.target_path, keep_frames=params.keep_frames)
