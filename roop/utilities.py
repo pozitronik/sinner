ed2b905797b6fe8cfe35e940b6437ae4f891c503
Pozitronik <antikillerxxl@gmail.com>
2023-06-30 12:53:13 +0400

    ignore mypy warnings

--- roop/utilities.py
+++ roop/utilities.py
@@ -169,7 +169,7 @@ def list_class_descendants(path: str, class_name: str) -> List['str']:
 
 
 def get_app_dir() -> str:
-    return os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))
+    return os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))  # type: ignore[type-var, return-value]
 
 
 def get_file_name(file_path: str) -> str:
