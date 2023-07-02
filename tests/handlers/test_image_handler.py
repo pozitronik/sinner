5a433778ddd385f3be343107b35645132ef932ec
Pozitronik <antikillerxxl@gmail.com>
2023-07-02 14:58:45 +0400

    Cover ImageHandler with tests

---
+++ tests/handlers/test_image_handler.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+from typing import Iterator
+
+import pytest
+from numpy import ndarray
+
+from sinner.handlers.frame.ImageHandler import ImageHandler
+from sinner.utilities import resolve_relative_path, is_image
+from tests.constants import TARGET_FC, IMAGE_SHAPE
+
+target_png: str = resolve_relative_path('../data/targets/target.png', __file__)
+target_mp4: str = resolve_relative_path('../data/targets/target.mp4', __file__)
+tmp_dir: str = resolve_relative_path('../data/temp', __file__)
+state_frames_dir: str = resolve_relative_path('../data/frames', __file__)
+result_mp4: str = os.path.join(tmp_dir, 'result.mp4')
+result_png: str = os.path.join(tmp_dir, 'result.png')
+
+
+def setup():
+    #  clean previous results, if exists
+    if os.path.exists(tmp_dir):
+        shutil.rmtree(tmp_dir)
+
+
+def test_init() -> None:
+    with pytest.raises(Exception):
+        ImageHandler(target_path='not_existed_directory')
+    with pytest.raises(Exception):
+        ImageHandler(target_path=target_mp4)
+    with pytest.raises(Exception):
+        ImageHandler(target_path=state_frames_dir)
+
+
+def get_test_object() -> ImageHandler:
+    return ImageHandler(target_path=target_png)
+
+
+def test_available() -> None:
+    assert get_test_object().available() is True
+
+
+def test_detect_fps() -> None:
+    assert 1 == get_test_object().detect_fps()
+
+
+def test_detect_fc() -> None:
+    assert 1 == get_test_object().detect_fc()
+
+
+def test_get_frames_paths() -> None:
+    frames_paths = get_test_object().get_frames_paths(path=tmp_dir)
+    assert 1 == len(frames_paths)
+    first_item = frames_paths[0]
+    assert (1, target_png) == first_item
+
+
+def test_extract_frame() -> None:
+    first_frame = get_test_object().extract_frame(1)
+    assert 1 == first_frame[0]
+    assert isinstance(first_frame[1], ndarray)
+    assert first_frame[1].shape == IMAGE_SHAPE
+
+
+def test_result() -> None:
+    assert os.path.exists(result_png) is False
+    assert get_test_object().result(from_dir=os.path.dirname(target_png), filename=result_png) is True
+    assert os.path.exists(result_png)
+    assert is_image(result_png)
+
+
+def tests_iterator() -> None:
+    test_object = get_test_object()
+    assert isinstance(test_object, Iterator)
+    frame_counter = 0
+    for frame_index in test_object:
+        assert (int, frame_index)
+        frame_counter += 1
+    assert frame_counter == 1
+
