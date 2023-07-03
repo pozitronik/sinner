d5fe58a28f119d532e9983a97b7b914dc521d014
Pozitronik <antikillerxxl@gmail.com>
2023-07-03 22:09:45 +0400

    face enhancer basic tests

---
+++ tests/processors/test_face_enhancer.py
@@ -0,0 +1,36 @@
+import multiprocessing
+
+from gfpgan import GFPGANer  # type: ignore[attr-defined]
+
+from sinner.face_analyser import FaceAnalyser
+from sinner.processors.frame.FaceEnhancer import FaceEnhancer
+from sinner.state import State
+from sinner.typing import Frame, Face
+from sinner.utilities import TEMP_DIRECTORY, resolve_relative_path, read_image
+from tests.constants import source_jpg, target_png, IMAGE_SHAPE
+
+
+def get_test_state() -> State:
+    return State(
+        source_path=source_jpg,
+        target_path=target_png,
+        frames_count=1,
+        temp_dir=resolve_relative_path(TEMP_DIRECTORY)
+    )
+
+
+def get_test_object() -> FaceEnhancer:
+    return FaceEnhancer(execution_providers=['CPUExecutionProvider'], execution_threads=multiprocessing.cpu_count(), max_memory=12, state=get_test_state())
+
+
+def test_init():
+    test_object = get_test_object()
+    assert (test_object, FaceEnhancer)
+    assert (test_object._face_analyser, FaceAnalyser)
+    assert (test_object._face_enhancer, GFPGANer)
+
+
+def test_process_frame():
+    processed_frame = get_test_object().process_frame(read_image(target_png))
+    assert (processed_frame, Frame)
+    assert processed_frame.shape == IMAGE_SHAPE
