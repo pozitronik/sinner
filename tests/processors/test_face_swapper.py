02e40b960c386c3d06611233ef3bbef5a0c0265d
Pozitronik <antikillerxxl@gmail.com>
2023-07-03 21:52:21 +0400

    face swapper tests

--- tests/processors/test_face_swapper.py
+++ tests/processors/test_face_swapper.py
@@ -1,8 +1,10 @@
 import multiprocessing
 
+from sinner.face_analyser import FaceAnalyser
 from sinner.processors.frame.FaceSwapper import FaceSwapper
 from sinner.state import State
-from sinner.utilities import TEMP_DIRECTORY, resolve_relative_path
+from sinner.typing import Frame, FaceSwapperType
+from sinner.utilities import TEMP_DIRECTORY, resolve_relative_path, read_image
 from tests.constants import source_jpg, target_png
 
 
@@ -15,14 +17,17 @@ def get_test_state() -> State:
     )
 
 
+def get_test_object() -> FaceSwapper:
+    return FaceSwapper(execution_providers=['CPUExecutionProvider'], execution_threads=multiprocessing.cpu_count(), max_memory=12, many_faces=False, source_path=source_jpg, state=get_test_state())
+
+
 def test_init():
-    test_object = FaceSwapper(execution_providers=['CPUExecutionProvider'], execution_threads=multiprocessing.cpu_count(), max_memory=12, many_faces=False, source_path=source_jpg, state=get_test_state())
+    test_object = get_test_object()
     assert (test_object, FaceSwapper)
+    assert (test_object._face_analyser, FaceAnalyser)
+    assert (test_object._face_swapper, FaceSwapperType)
 
 
 def test_process_frame():
-    pass
-
-
-def test_swap_face():
-    pass
+    processed_frame = get_test_object().process_frame(read_image(target_png))
+    assert (processed_frame, Frame)
