d3b8ec0052cdb6a87b2d369bfe0101244b91b0df
Pozitronik <antikillerxxl@gmail.com>
2023-07-04 23:55:23 +0400

    init_slider() method

--- sinner/Preview.py
+++ sinner/Preview.py
@@ -3,7 +3,7 @@ import cv2
 
 from PIL import Image, ImageTk
 from PIL.Image import Resampling
-from customtkinter import CTkLabel, CTk
+from customtkinter import CTkLabel, CTk, CTkSlider
 
 from sinner.core import Core
 from sinner.handlers.frame.BaseFrameHandler import BaseFrameHandler
@@ -14,6 +14,7 @@ from sinner.utilities import is_image, is_video
 class Preview:
     core: Core
     preview_label: CTkLabel
+    preview_slider: CTkSlider
 
     def __init__(self, core: Core):
         self.core = core
@@ -29,22 +30,25 @@ class Preview:
         self.preview_label.drop_target = True
 
         # self.preview_label.bind('<Configure>', lambda event: self.resize_image(event, self.preview_label))
-        preview_slider = ctk.CTkSlider(root, to=0, command=lambda frame_value: self.update_preview(self.preview_label, frame_value))
-        self.preview_label.bind("<Double-Button-1>", lambda event: self.update_preview(self.preview_label, int(preview_slider.get()), True))
-        self.preview_label.bind("<Button-2>", lambda event: self.change_source(int(preview_slider.get())))
-        self.preview_label.bind("<Button-3>", lambda event: self.change_target(int(preview_slider.get())))
+        self.preview_slider = CTkSlider(root, to=0, command=lambda frame_value: self.update_preview(self.preview_label, frame_value))
+        self.preview_label.bind("<Double-Button-1>", lambda event: self.update_preview(self.preview_label, int(self.preview_slider.get()), True))
+        self.preview_label.bind("<Button-2>", lambda event: self.change_source(int(self.preview_slider.get())))
+        self.preview_label.bind("<Button-3>", lambda event: self.change_target(int(self.preview_slider.get())))
         self.preview_label.pack(fill='both', expand=True)
 
+        self.init_slider()
+
+        self.update_preview(self.preview_label, int(self.preview_slider.get()))
+        return root
+
+    def init_slider(self) -> None:
         if is_image(self.core.params.target_path):
-            preview_slider.pack_forget()
+            self.preview_slider.pack_forget()
         if is_video(self.core.params.target_path):
             video_frame_total = BaseFrameHandler.create(handler_name=self.core.params.frame_handler, target_path=self.core.params.target_path).fc
-            preview_slider.configure(to=video_frame_total)
-            preview_slider.pack(fill='x')
-            preview_slider.set(video_frame_total / 2)
-
-        self.update_preview(self.preview_label, int(preview_slider.get()))
-        return root
+            self.preview_slider.configure(to=video_frame_total)
+            self.preview_slider.pack(fill='x')
+            self.preview_slider.set(video_frame_total / 2)
 
     def change_source(self, frame_number: int = 0) -> None:
         self.core.change_source(ctk.filedialog.askopenfilename(title='Select a source'))
@@ -53,6 +57,7 @@ class Preview:
     def change_target(self, frame_number: int = 0) -> None:
         self.core.change_target(ctk.filedialog.askopenfilename(title='Select a target'))
         self.update_preview(self.preview_label, frame_number, True)
+        self.init_slider()
 
     @staticmethod
     def render_image_preview(frame: Frame) -> ImageTk.PhotoImage:
