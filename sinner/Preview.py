62210cffefe1069a49047064d40ed8e1bfc90307
Pozitronik <antikillerxxl@gmail.com>
2023-07-05 15:02:34 +0400

    Improve preview window

--- sinner/Preview.py
+++ sinner/Preview.py
@@ -1,4 +1,5 @@
-import customtkinter as ctk
+from tkinter import filedialog, Entry, LEFT, Button, Label, END, Frame, BOTH, RIGHT, StringVar, W, EW, E, NE, NW
+
 import cv2
 
 from PIL import Image, ImageTk
@@ -7,55 +8,84 @@ from customtkinter import CTkLabel, CTk, CTkSlider
 
 from sinner.core import Core
 from sinner.handlers.frame.BaseFrameHandler import BaseFrameHandler
-from sinner.typing import Frame
 from sinner.utilities import is_image, is_video
 
 
 class Preview:
     core: Core
+    root: CTk
     preview_label: CTkLabel
     preview_slider: CTkSlider
 
     def __init__(self, core: Core):
         self.core = core
+        self.root = CTk()
+        self.root.title('Preview')
+        self.root.protocol('WM_DELETE_WINDOW', lambda: self.destroy())
+        self.root.resizable(width=True, height=True)
+        self.current_position = StringVar()
 
     def show(self) -> CTk:
-        root = ctk.CTk()
-        root.title('Preview')
-        root.protocol('WM_DELETE_WINDOW', lambda: self.destroy())
-        root.configure()
-        root.resizable(width=True, height=True)
+        self.init_preview()
+        self.init_slider()
+        self.init_open_source_control()
+        self.init_open_target_control()
+        self.update_preview(self.preview_label, int(self.preview_slider.get()))
+        return self.root
 
-        self.preview_label = CTkLabel(root, text='')
-        self.preview_label.drop_target = True
+    def init_preview(self) -> None:
+        self.preview_label = CTkLabel(self.root, text='')
 
         # self.preview_label.bind('<Configure>', lambda event: self.resize_image(event, self.preview_label))
-        self.preview_slider = CTkSlider(root, to=0, command=lambda frame_value: self.update_preview(self.preview_label, frame_value))
+
         self.preview_label.bind("<Double-Button-1>", lambda event: self.update_preview(self.preview_label, int(self.preview_slider.get()), True))
         self.preview_label.bind("<Button-2>", lambda event: self.change_source(int(self.preview_slider.get())))
         self.preview_label.bind("<Button-3>", lambda event: self.change_target(int(self.preview_slider.get())))
         self.preview_label.pack(fill='both', expand=True)
 
-        self.init_slider()
-
-        self.update_preview(self.preview_label, int(self.preview_slider.get()))
-        return root
-
     def init_slider(self) -> None:
+        frame = Frame(self.root, borderwidth=2)
+        self.preview_slider = CTkSlider(frame, to=0, command=lambda frame_value: self.update_preview(self.preview_label, frame_value))
         if is_image(self.core.params.target_path):
             self.preview_slider.pack_forget()
         if is_video(self.core.params.target_path):
             video_frame_total = BaseFrameHandler.create(handler_name=self.core.params.frame_handler, target_path=self.core.params.target_path).fc
             self.preview_slider.configure(to=video_frame_total)
-            self.preview_slider.pack(fill='x')
+            self.preview_slider.pack(anchor=NW, side=LEFT, expand=True, fill=BOTH)
             self.preview_slider.set(video_frame_total / 2)
 
+        current_position_label = Label(frame, textvariable=self.current_position)
+        current_position_label.pack(anchor=NE, side=LEFT)
+
+        frame.pack(fill='x')
+
+    def init_open_source_control(self) -> None:
+        frame = Frame(self.root, borderwidth=2)
+        path_entry = Entry(frame)
+        path_entry.insert(END, self.core.params.source_path)
+        path_entry.pack(side=LEFT, expand=True, fill=BOTH)
+
+        open_button = Button(frame, text="Browse for source", width=20, command=lambda: self.change_source(int(self.preview_slider.get())))
+        open_button.pack(side=RIGHT)
+        frame.pack(fill='x')
+
+    def init_open_target_control(self) -> None:
+        frame = Frame(self.root, borderwidth=2)
+
+        path_entry = Entry(frame)
+        path_entry.insert(END, self.core.params.target_path)
+        path_entry.pack(side=LEFT, expand=True, fill=BOTH)
+
+        open_button = Button(frame, text="Browse for target", width=20, command=lambda: self.change_target(int(self.preview_slider.get())))
+        open_button.pack(side=LEFT)
+        frame.pack(fill='x')
+
     def change_source(self, frame_number: int = 0) -> None:
-        if self.core.change_source(ctk.filedialog.askopenfilename(title='Select a source')):
+        if self.core.change_source(filedialog.askopenfilename(title='Select a source')):
             self.update_preview(self.preview_label, frame_number, True)
 
     def change_target(self, frame_number: int = 0) -> None:
-        if self.core.change_target(ctk.filedialog.askopenfilename(title='Select a target')):
+        if self.core.change_target(filedialog.askopenfilename(title='Select a target')):
             self.update_preview(self.preview_label, frame_number, True)
             self.init_slider()
 
@@ -71,6 +101,7 @@ class Preview:
         image = ImageTk.PhotoImage(pil_image)
         preview_label.configure(image=image)
         preview_label.image = image
+        self.current_position.set(f'{int(frame_number)}/{self.preview_slider.cget("to")}')
 
     @staticmethod
     def destroy():
