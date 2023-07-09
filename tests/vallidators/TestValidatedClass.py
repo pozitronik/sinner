b79962c7af5c910aff2cac95cc42cb70414e4e38
Pozitronik <antikillerxxl@gmail.com>
2023-07-09 14:51:40 +0400

    issue #4 do tests

--- tests/vallidators/TestValidatedClass.py
+++ tests/vallidators/TestValidatedClass.py
@@ -3,9 +3,31 @@ from sinner.processors.BaseValidatedClass import BaseValidatedClass, Rules
 DEFAULT_VALUE = 42
 
 
+class TestDefaultValidation(BaseValidatedClass):
+    default_parameter: int = None
+    parameter_name: int = None
+
+    def rules(self) -> Rules:
+        return [
+            {'parameter': 'default_parameter', 'default': DEFAULT_VALUE},  # class.default_value = DEFAULT_VALUE
+            {'parameter': 'parameter-name', 'default': DEFAULT_VALUE},  # class.parameter_name = DEFAULT_VALUE
+        ]
+
+
+class TestRequiredValidation(BaseValidatedClass):
+    required_parameter: str = None
+    default_required_parameter: int = None
+    required_default_parameter: int = None
+
+    def rules(self) -> Rules:
+        return [
+            {'parameter': 'required_parameter', 'required': True},  # Ok, if '--required_parameter' passed, else ValueRequired exception
+            {'parameter': 'default_required_parameter', 'default': DEFAULT_VALUE, 'required': True},  # class.default_required_parameter = DEFAULT_VALUE, no error
+            {'parameter': 'required_default_parameter', 'required': True, 'default': DEFAULT_VALUE},  # required validation made before default, so ValueRequired exception here (?)
+        ]
+
+
 class TestValidatedClass(BaseValidatedClass):
-    default_parameter: int
-    parameter_name: int
     required_parameter: str
     default_required_parameter: int
     required_default_parameter: int
--- tests/vallidators/test_validators.py
+++ tests/vallidators/test_validators.py
@@ -1,13 +1,56 @@
-from sinner.processors.BaseValidatedClass import BaseValidatedClass
+from argparse import ArgumentParser, Namespace
+
+from sinner.Parameters import Parameters
+from tests.vallidators.TestValidatedClass import DEFAULT_VALUE, TestDefaultValidation, TestRequiredValidation
 
 
-def get_test_object() -> 'TestValidatedClass':
-    return BaseValidatedClass()
+def command_line_to_namespace(cmd_params: str) -> Namespace:
+    parser = ArgumentParser()
+    args, unknown_args = parser.parse_known_args(cmd_params)
+    for argument in unknown_args:
+        key, value = Parameters.parse_argument(argument)
+        setattr(args, key, value)
+    return args
 
 
 def test_default_validator() -> None:
-    pass
+    parameters: Namespace = command_line_to_namespace('')
+    test_object = TestDefaultValidation()
+
+    assert test_object.default_parameter is None
+    assert test_object.parameter_name is None
+
+    assert test_object.load(parameters) is True
+    assert test_object.default_parameter == DEFAULT_VALUE
+    assert test_object.parameter_name == DEFAULT_VALUE
+    assert test_object.errors == []
+
+    # test if default validator doesn't overwrite existing values
+    test_object.default_parameter = 14
+    test_object.parameter_name = 88
+    assert test_object.load(parameters) is True
+    assert test_object.default_parameter == 14
+    assert test_object.parameter_name == 88
+    assert test_object.errors == []
 
 
 def test_required_validator() -> None:
-    pass
+    parameters: Namespace = command_line_to_namespace('')
+    test_object = TestRequiredValidation()
+    assert test_object.required_parameter is None
+    assert test_object.default_required_parameter is None
+    assert test_object.default_required_parameter is None
+
+    # nothing should be changed, if loading unsuccessful
+    assert test_object.load(parameters) is False
+    assert test_object.required_parameter is None
+    assert test_object.default_required_parameter is None
+    assert test_object.default_required_parameter is None
+    assert test_object.errors != []
+
+    # ignore validation on load
+    assert test_object.load(parameters, validate=False) is True
+
+    assert test_object.required_parameter is None
+    assert test_object.default_required_parameter == DEFAULT_VALUE
+    assert test_object.required_default_parameter is None
