4066b32d58e100081003e2589e5cfe12be9c0b0d
Pozitronik <antikillerxxl@gmail.com>
2023-07-11 21:59:07 +0400

    issue #4 Next test stub

--- tests/run_test.py
+++ tests/run_test.py
@@ -5,10 +5,18 @@ from sinner.Parameters import Parameters
 from sinner.Core import Core
 from sinner.utilities import limit_resources
 from sinner.validators.LoaderException import LoadingException
+from tests.constants import target_png
 
 
 def test_one() -> None:
     params = Parameters()
     limit_resources(params.max_memory)
     with pytest.raises(LoadingException):
-        Core(parameters=params.parameters)
+        Core(parameters=params.parameters).run()
+
+
+def test_two() -> None:
+    params = Parameters(f'--target_path={target_png}')
+    limit_resources(params.max_memory)
+    Core(parameters=params.parameters).run()
+
