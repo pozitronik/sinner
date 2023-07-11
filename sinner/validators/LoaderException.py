62c2c738e6df4c37455bc6d5eb6797846d978be2
Pozitronik <antikillerxxl@gmail.com>
2023-07-11 11:32:41 +0400

    issue #4 Introduce LoaderException

---
+++ sinner/validators/LoaderException.py
@@ -0,0 +1,17 @@
+from colorama import Fore, Back, Style
+
+from sinner.validators.BaseValidator import BaseValidator
+
+
+class LoaderException(Exception):
+    message: str
+    validated_object: object
+    attribute: str
+
+    def __init__(self, message: str, validated_object: object, attribute: str):
+        self.message = message
+        self.validated_object = validated_object
+        self.attribute = attribute
+
+    def __str__(self) -> str:
+        return f"{Fore.BLACK}{Back.RED}{self.validated_object.__class__.__name__}{Back.RESET}{Fore.RESET}: {self.message}@{Style.BRIGHT}{self.attribute}{Style.RESET_ALL}"
