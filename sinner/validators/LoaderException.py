97ae13e296e8ab53ed15fbc8b6dd867b277e473c
Pozitronik <antikillerxxl@gmail.com>
2023-07-11 21:51:25 +0400

    issue #4 Introduce LoadingException for better error output and handling

--- sinner/validators/LoaderException.py
+++ sinner/validators/LoaderException.py
@@ -1,6 +1,10 @@
+import os
+from typing import List
+
 from colorama import Fore, Back, Style
 
 
+# The exception inside a loader
 class LoaderException(Exception):
     message: str
     validated_object: object
@@ -13,3 +17,22 @@ class LoaderException(Exception):
 
     def __str__(self) -> str:
         return f"{Fore.BLACK}{Back.RED}{self.validated_object.__class__.__name__}{Back.RESET}{Fore.RESET}: {self.message} ({Style.BRIGHT}{self.attribute}{Style.RESET_ALL})"
+
+
+# The user exception, when loading is not success
+class LoadingException(Exception):
+    errors: List[dict[str, str]] = []
+
+    def __init__(self, errors: List[dict[str, str]]):
+        self.errors = errors
+
+    def __str__(self) -> str:
+        validation_errors: List[str] = []
+        attributes_help: List[str] = []
+        for error in self.errors:
+            validation_errors.append(f"Parameter {Fore.YELLOW}--{error['attribute']}{Fore.RESET} {Fore.RED}{error['error']}{Fore.RESET} in module {Fore.CYAN}{error['module']}{Fore.RESET}")
+            if error['help'] is not None:
+                attributes_help.append(f"{Style.BRIGHT}{Fore.YELLOW}--{error['attribute']}{Fore.RESET}={error['help']}{Style.RESET_ALL}")
+            else:
+                attributes_help.append(f"{Style.BRIGHT}{Fore.YELLOW}--{error['attribute']}{Fore.RESET}{Style.RESET_ALL}")
+        return os.linesep.join(validation_errors) + os.linesep + ' '.join(attributes_help)
