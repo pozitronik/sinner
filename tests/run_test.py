7c453f9030cf536ae13af7a2f03d5e6ce2b7f133
Pozitronik <antikillerxxl@gmail.com>
2023-07-12 15:01:33 +0400

    issue #4 New run test

--- tests/run_test.py
+++ tests/run_test.py
@@ -5,18 +5,25 @@ from sinner.Parameters import Parameters
 from sinner.Core import Core
 from sinner.utilities import limit_resources
 from sinner.validators.LoaderException import LoadingException
-from tests.constants import target_png
+from tests.constants import target_png, source_jpg
 
 
 def test_one() -> None:
     params = Parameters()
     limit_resources(params.max_memory)
     with pytest.raises(LoadingException):
-        Core(parameters=params.parameters).run()
+        Core(parameters=params.parameters).run()  # target path is fucked up
 
 
 def test_two() -> None:
-    params = Parameters(f'--target_path="{target_png}"')
+    params = Parameters(f'--target_path="{target_png}" --source_path=no_such_file')
     limit_resources(params.max_memory)
-    Core(parameters=params.parameters).run()
+    with pytest.raises(LoadingException):
+        Core(parameters=params.parameters).run()  # source path is fucked up
 
+
+def test_three() -> None:
+    params = Parameters(f'--target_path="{target_png}" --source_path="{source_jpg}"')
+    limit_resources(params.max_memory)
+    # with pytest.raises(LoadingException):
+    Core(parameters=params.parameters).run()
