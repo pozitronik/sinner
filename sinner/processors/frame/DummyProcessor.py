fbc930509eb21343f03290f15770fdf0f87f17d9
Pozitronik <antikillerxxl@gmail.com>
2023-07-13 13:01:45 +0400

    issue #4 implement abstract method in the test class

--- sinner/processors/frame/DummyProcessor.py
+++ sinner/processors/frame/DummyProcessor.py
@@ -4,5 +4,8 @@ from sinner.typing import Frame
 
 class DummyProcessor(BaseFrameProcessor):
 
+    def suggest_output_path(self) -> str:
+        pass
+
     def process_frame(self, temp_frame: Frame) -> Frame:
         return temp_frame
