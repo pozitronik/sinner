66e2d8853cf5d1368f9da0486ef9fb8a2ab172cd
Pozitronik <antikillerxxl@gmail.com>
2023-07-13 19:16:06 +0400

    issue #4 DummyProcessor fixed

--- sinner/processors/frame/DummyProcessor.py
+++ sinner/processors/frame/DummyProcessor.py
@@ -1,11 +1,33 @@
+import os
+
 from sinner.processors.frame.BaseFrameProcessor import BaseFrameProcessor
 from sinner.typing import Frame
+from sinner.validators.AttributeLoader import Rules
 
 
 class DummyProcessor(BaseFrameProcessor):
 
+    def rules(self) -> Rules:
+        return super().rules() + [
+            {
+                'parameter': 'target_path',
+                'required': True,
+                'help': 'Select the target file (image or video) or the directory'
+            },
+            {
+                'parameter': 'output_path',
+                'default': lambda: self.suggest_output_path(),
+                'help': 'Select an output file or a directory'
+            },
+        ]
+
     def suggest_output_path(self) -> str:
-        pass
+        target_name, target_extension = os.path.splitext(os.path.basename(self.target_path))
+        if self.output_path is None:
+            return os.path.join(os.path.dirname(self.target_path), 'dummy-' + target_name + target_extension)
+        if os.path.isdir(self.output_path):
+            return os.path.join(self.output_path, 'dummy-' + target_name + target_extension)
+        return self.output_path
 
     def process_frame(self, temp_frame: Frame) -> Frame:
         return temp_frame
