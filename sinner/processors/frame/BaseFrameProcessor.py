725f2962a44682592e024aa009f4073c706e248b
Pozitronik <antikillerxxl@gmail.com>
2023-07-14 15:29:40 +0400

    issue #4 unused import

--- sinner/processors/frame/BaseFrameProcessor.py
+++ sinner/processors/frame/BaseFrameProcessor.py
@@ -11,7 +11,7 @@ from sinner.handlers.frame.BaseFrameHandler import BaseFrameHandler
 from sinner.validators.AttributeLoader import AttributeLoader, Rules
 from sinner.State import State
 from sinner.typing import Frame, FramesDataType, FrameDataType, NumeratedFrame
-from sinner.utilities import update_status, load_class, get_mem_usage, read_image, suggest_execution_threads, suggest_execution_providers, decode_execution_providers, suggest_max_memory
+from sinner.utilities import load_class, get_mem_usage, read_image, suggest_execution_threads, suggest_execution_providers, decode_execution_providers, suggest_max_memory
 
 
 class BaseFrameProcessor(ABC, AttributeLoader):
