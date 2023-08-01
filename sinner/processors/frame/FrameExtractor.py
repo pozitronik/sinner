c080b0e3335645fcf3629cfc1b548a617df94d25
Pozitronik <antikillerxxl@gmail.com>
2023-08-01 17:10:35 +0400

    fixed: FrameExtractor also reports about processing state

--- sinner/processors/frame/FrameExtractor.py
+++ sinner/processors/frame/FrameExtractor.py
@@ -36,6 +36,11 @@ class FrameExtractor(BaseFrameProcessor):
 
     def process(self, frames: BaseFrameHandler, state: State, desc: str = 'Processing', set_progress: Callable[[int], None] | None = None) -> None:
         state.path = os.path.abspath(os.path.join(state.temp_dir, self.__class__.__name__, os.path.basename(self.target_path)))
+        if state.is_finished:  # it is a little overcomplicated, need to think, how to force Core make a lookahead for state path into a Processor
+            self.update_status(f'Processing with {state.processor_name} already done ({state.processed_frames_count}/{state.frames_count})')
+        else:
+            if state.is_started:
+                self.update_status(f'Temp resources for this target already exists with {state.processed_frames_count} frames processed, continue processing with {state.processor_name}')
         frames.get_frames_paths(state.path, (state.processed_frames_count, None))
         if not state.final_check():
             raise Exception("Something went wrong on processed frames check")
