8f9679149628dc8802f8f33bad9d8ed17e9acdad
Pozitronik <antikillerxxl@gmail.com>
2023-08-02 22:26:41 +0400

    issue #45 ini-files support added

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,12 +3,12 @@ from tests.constants import test_config
 
 
 def test_ini() -> None:
-    params = Parameters(f'--ini={test_config}')
+    params = Parameters(f'--ini="{test_config}"').parameters
     assert params.test_key == 'test_value'
 
 
 def test_ini_and_cmd_line() -> None:
-    params = Parameters(f'--ini={test_config} --max-memory=28')
+    params = Parameters(f'--ini="{test_config}" --max-memory=28').parameters
     assert params.test_key == 'test_value'
     assert params.max_memory == '28'
-    assert params.many_faces == True
+    assert bool(params.many_faces) is True
