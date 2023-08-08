2af9857ccb993c70967779eda1ea7e6794c877a5
Pozitronik <antikillerxxl@gmail.com>
2023-08-08 14:46:19 +0400

    issue #32 Add all documented class into the list

--- sinner/validators/AttributeDocumenter.py
+++ sinner/validators/AttributeDocumenter.py
@@ -2,11 +2,39 @@ from typing import Type, List, Dict
 
 from colorama import Style, Fore
 
+from sinner.Benchmark import Benchmark
 from sinner.Core import Core
+from sinner.Preview import Preview
+from sinner.State import State
+from sinner.Status import Status
+from sinner.handlers.frame.CV2VideoHandler import CV2VideoHandler
+from sinner.handlers.frame.DirectoryHandler import DirectoryHandler
+from sinner.handlers.frame.FFmpegVideoHandler import FFmpegVideoHandler
+from sinner.handlers.frame.ImageHandler import ImageHandler
+from sinner.handlers.frame.VideoHandler import VideoHandler
+from sinner.processors.frame.FaceEnhancer import FaceEnhancer
+from sinner.processors.frame.FaceSwapper import FaceSwapper
+from sinner.processors.frame.FrameExtractor import FrameExtractor
+from sinner.processors.frame.FrameResizer import FrameResizer
+from sinner.processors.frame.ResultProcessor import ResultProcessor
 from sinner.validators import AttributeLoader
 
 DocumentedClasses: List[Type[AttributeLoader]] = [
-    Core
+    Core,
+    Status,
+    State,
+    Preview,
+    Benchmark,
+    FaceEnhancer,
+    FaceSwapper,
+    FrameExtractor,
+    FrameResizer,
+    ResultProcessor,
+    CV2VideoHandler,
+    DirectoryHandler,
+    FFmpegVideoHandler,
+    ImageHandler,
+    VideoHandler
 ]
 
 
