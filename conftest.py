96ec49af44032bbd8be9eab7f1bbb1282f95c655
Pozitronik <antikillerxxl@gmail.com>
2023-08-18 23:42:03 +0400

    issue #32 conftest.py for pytest pre-hooks

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import os
+
+
+def pytest_sessionstart(session):
+    #  try to avoid tkl initialization errors while testing in GitHub CI
+    if 'CI' in os.environ and 'DISPLAY' not in os.environ:
+        os.environ.__setitem__('DISPLAY', ':0.0')
