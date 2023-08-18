06437f58340648f36b0a630e64c0e044ad935512
Pozitronik <antikillerxxl@gmail.com>
2023-08-18 23:54:58 +0400

    issue #32 Install xvfb in CI to avoid tkl failure

--- conftest.py
+++ conftest.py
@@ -4,4 +4,5 @@ import os
 def pytest_sessionstart(session):
     #  try to avoid tkl initialization errors while testing in GitHub CI
     if 'CI' in os.environ and 'DISPLAY' not in os.environ:
-        os.environ.__setitem__('DISPLAY', ':0.0')
+        os.system('Xvfb :1 -screen 0 1600x1200x16  &')
+        os.environ.__setitem__('DISPLAY', ':1.0')
