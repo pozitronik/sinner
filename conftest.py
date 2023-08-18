7165dff7924c884852b2601ce5d3b7f108d720c3
Pozitronik <antikillerxxl@gmail.com>
2023-08-19 00:08:40 +0400

    issue #32 Move CI trick to ci.yml

--- conftest.py
+++ conftest.py
@@ -1,8 +1,2 @@
-import os
-
-
 def pytest_sessionstart(session):
-    #  try to avoid tkl initialization errors while testing in GitHub CI
-    if 'CI' in os.environ and 'DISPLAY' not in os.environ:
-        os.system('Xvfb :1 -screen 0 1600x1200x16  &')
-        os.environ.__setitem__('DISPLAY', ':1.0')
+    pass
