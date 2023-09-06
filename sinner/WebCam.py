1b3d180f69c893f209d747bd9ddc81e49dd43417
Pozitronik <antikillerxxl@gmail.com>
2023-09-06 18:39:19 +0400

    #13 Imroving camera module

--- sinner/WebCam.py
+++ sinner/WebCam.py
@@ -6,10 +6,32 @@ import cv2
 from cv2 import VideoCapture
 from pyvirtualcam import Camera
 
-from sinner.Status import Status
+from sinner.Status import Status, Mood
+from sinner.handlers.frame.CV2VideoHandler import CV2VideoHandler
 from sinner.processors.frame.BaseFrameProcessor import BaseFrameProcessor
-from sinner.utilities import list_class_descendants, resolve_relative_path
+from sinner.typing import Frame
+from sinner.utilities import list_class_descendants, resolve_relative_path, is_image
 from sinner.validators.AttributeLoader import Rules
+
+
+class ImageCamera(VideoCapture):
+    _frame: Frame
+
+    def __init__(self, image: str, width: int, height: int):
+        super().__init__()
+        self._frame = CV2VideoHandler.read_image(image)
+        self._frame = cv2.resize(self._frame, (width, height))
+
+    def read(self, image: cv2.typing.MatLike | None = None) -> tuple[bool, Frame]:
+        return True, self._frame
+
+
+class NoDevice(Camera):
+    def send(self, frame: Frame) -> None:
+        return None
+
+    def sleep_until_next_frame(self) -> None:
+        return None
 
 
 class WebCam(Status):
@@ -19,12 +41,13 @@ class WebCam(Status):
 
     frame_processor: List[str]
     preview: bool
-    input_device: int
+    input_device: int | str
     output_device: str | None
     width: int
     height: int
     fps: int
     print_fps: bool
+    auto_restart: bool
 
     _camera_input: VideoCapture
     _processors: List[BaseFrameProcessor] = []
@@ -49,14 +72,14 @@ class WebCam(Status):
                 'parameter': ['input', 'input-device'],
                 'attribute': 'input_device',
                 'default': 0,
-                'help': 'Input camera index (ignore, if you have one camera)'
+                'help': 'Input camera index (ignore, if you have one camera). Pass a path to an image file to use it as input.'
             },
             {
-                'parameter': ['output', 'output-device'],
+                'parameter': ['device', 'output-device'],
                 'default': None,
                 'choices': self.list_available_output_devices(),
                 'attribute': 'output_device',
-                'help': 'Output device name (e.g. virtual camera device to use), ignore to the first available device'
+                'help': 'Output device name (e.g. virtual camera device to use), ignore to the first available device or use "no" to skip output'
             },
             {
                 'parameter': 'width',
@@ -79,6 +102,11 @@ class WebCam(Status):
                 'help': 'Print frame rate every second'
             },
             {
+                'parameter': ['auto-restart', 'restart'],
+                'default': True,
+                'help': 'Try to restart input camera on error (may help with buggy drivers/hardware)'
+            },
+            {
                 'module_help': 'The virtual camera module'
             }
         ]
@@ -86,11 +114,12 @@ class WebCam(Status):
     def __init__(self, parameters: Namespace):
         self.parameters = parameters
         super().__init__(parameters)
-        self._camera_input = self.open_camera()
-        self.update_status("Camera input is opened")
-
-        self._device = Camera(width=self.width, height=self.height, fps=self.fps, device=self.output_device, backend=self.output_device, print_fps=self.print_fps)
-        self.update_status(f"Virtual device camera is created as {self.output_device} with {self.width}x{self.height}@{self.fps}fps output")
+        self.open_camera()
+        if self.output_device == 'no':  # creates a dummy output
+            self._device = NoDevice(width=self.width, height=self.height, fps=self.fps)
+        else:
+            self._device = Camera(width=self.width, height=self.height, fps=self.fps, device=self.output_device, backend=self.output_device, print_fps=self.print_fps)
+            self.update_status(f"Virtual device camera is created as {self.output_device} with {self.width}x{self.height}@{self.fps}fps output")
 
         for processor_name in self.frame_processor:
             self._processors.append(BaseFrameProcessor.create(processor_name, self.parameters))
@@ -100,21 +129,34 @@ class WebCam(Status):
             while not self.stop:
                 ret, frame = self._camera_input.read()
                 if not ret:
-                    raise Exception(f"Error reading frame from camera")
+                    self.update_status(f"Error reading input from camera", mood=Mood.BAD)
+                    if self.auto_restart:
+                        self.update_status(f"Reopening camera device")
+                        self._camera_input.release()
+                        self.open_camera()
+                    continue
                 for processor in self._processors:
                     frame = processor.process_frame(frame)
+                if self.preview:
+                    cv2.imshow('Frame', frame)
                 camera.send(frame)
                 camera.sleep_until_next_frame()
 
     def open_camera(self) -> VideoCapture:
-        cap = cv2.VideoCapture(self.input_device)
-        if not cap.isOpened():
+        if is_image(self.input_device):
+            self._camera_input = ImageCamera(self.input_device, self.width, self.height)
+            self.update_status(f"Using image {self.input_device} as camera input")
+            return self._camera_input
+
+        self._camera_input = cv2.VideoCapture(self.input_device)
+        if not self._camera_input.isOpened():
             raise Exception(f"Error opening camera {self.input_device}")
-        return cap
+        self.update_status(f"Camera input is opened at device={self.input_device}")
+        return self._camera_input
 
     @staticmethod
     def list_available_output_devices() -> List[str]:
-        devices: List[str] = []
+        devices: List[str] = ['no']
         if sys.platform == 'linux':
             devices.append('v4l2loopback')
         if sys.platform == 'win32':
