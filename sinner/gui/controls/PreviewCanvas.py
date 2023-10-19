45a14d121a651145b6b243a053eb084a37127de5
Pozitronik <antikillerxxl@gmail.com>
2023-10-19 14:23:05 +0400

    issue #18 Add a method to show raw frame

--- sinner/gui/controls/PreviewCanvas.py
+++ sinner/gui/controls/PreviewCanvas.py
@@ -1,5 +1,11 @@
 from tkinter import Canvas
+
+import cv2
+from PIL import ImageTk, Image
 from PIL.ImageTk import PhotoImage
+
+from sinner.gui.controls.FrameThumbnail import FrameThumbnail
+from sinner.typing import Frame
 
 
 class PreviewCanvas(Canvas):
@@ -9,10 +15,18 @@ class PreviewCanvas(Canvas):
     def photo_image(self) -> PhotoImage | None:
         return self.photo
 
-    @property.setter
+    @photo_image.setter
     def photo_image(self, image: PhotoImage | None) -> None:
         try:  # todo
             self.create_image(self.winfo_width() // 2, self.winfo_height() // 2, image=image)
             self.photo = image
         except Exception as e:
             pass
+
+    def show_frame(self, frame: Frame | None = None) -> None:
+        if frame is None:
+            self.photo_image = None
+        else:
+            image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            image = FrameThumbnail.resize_image(image, (self.winfo_width(), self.winfo_height()))
+            self.photo_image = ImageTk.PhotoImage(image)
