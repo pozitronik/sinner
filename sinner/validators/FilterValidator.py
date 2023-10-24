d5200e20e48c6fde892091887134b4b2efa843d8
Pozitronik <antikillerxxl@gmail.com>
2023-10-24 16:44:22 +0400

    issue #84 introduce FilterValidator validator to filter parameters

---
+++ sinner/validators/FilterValidator.py
@@ -0,0 +1,26 @@
+import inspect
+
+from sinner.validators.BaseValidator import BaseValidator
+from sinner.validators.ErrorDTO import ErrorDTO
+from sinner.validators.ValidatorException import ValidatorException
+
+
+# applies a filter on the input value and assigns it back to the attribute being validated.
+class FilterValidator(BaseValidator):
+
+    def validate(self, validated_object: object, attribute: str) -> ErrorDTO | None:
+        filter_method = self.arguments['value']
+        if callable(filter_method):
+            try:
+                callable_parameters_count = len(inspect.signature(filter_method).parameters)
+                if 0 == callable_parameters_count:
+                    value = filter_method()
+                elif 1 == callable_parameters_count:
+                    value = filter_method(attribute)
+                else:
+                    raise ValidatorException(f'More than 1 attribute is not allowed in filter lambdas ({callable_parameters_count} are present) for {attribute} attribute', validated_object, self)
+                setattr(validated_object, attribute, value)
+                return None
+            except Exception as exception:
+                raise ValidatorException(f'{type(exception).__name__} with message `{str(exception)}` when retrieve callable value for {attribute} attribute', validated_object, self)
+        raise ValidatorException(f'Callable expected in the {attribute} attribute', validated_object, self)
