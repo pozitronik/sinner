117e12b84732c7796632ffed6fdc7dd5666bb62a
Pozitronik <antikillerxxl@gmail.com>
2023-10-24 14:42:41 +0400

    issue #18 Implement position label right inside position widget

--- sinner/gui/controls/NavigateSlider.py
+++ sinner/gui/controls/NavigateSlider.py
@@ -1,7 +1,25 @@
+from tkinter import Label, StringVar, NE, RIGHT
+from typing import Union, Callable
+
 from customtkinter import CTkSlider
 
 
 class NavigateSlider(CTkSlider):
+    _position_label: Label | None = None
+    _current_position: StringVar = None
+    _cmd: Union[Callable[[float], None], None] = None
+
+    def __init__(self, master: any, **kwargs):
+        super().__init__(master, **kwargs)
+        self._current_position = StringVar()
+        self._position_label = Label(master)
+        self._position_label.pack(anchor=NE, side=RIGHT)
+        self._position_label.configure(textvariable=self._current_position)
+
+    def _clicked(self, event=None) -> None:
+        super()._clicked(event)
+        if self._position_label:
+            self._current_position.set(f'{self.position}/{self._to}')
 
     @property
     def to(self) -> int:
