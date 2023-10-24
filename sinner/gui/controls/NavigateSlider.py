97f6d4f0304f0e39d5229468308563394a0ff89b
Pozitronik <antikillerxxl@gmail.com>
2023-10-24 14:54:47 +0400

    issue #18 introduce container frame to put widget components inside

--- sinner/gui/controls/NavigateSlider.py
+++ sinner/gui/controls/NavigateSlider.py
@@ -1,23 +1,34 @@
-from tkinter import Label, StringVar, NE, RIGHT
-from typing import Union, Callable
+from tkinter import Label, StringVar, NE, RIGHT, Frame, X
+from typing import Union, Callable, Any
 
 from customtkinter import CTkSlider
 
 
 class NavigateSlider(CTkSlider):
+    _container = Frame
     _position_label: Label | None = None
     _current_position: StringVar = None
     _cmd: Union[Callable[[float], None], None] = None
 
     def __init__(self, master: any, **kwargs):
-        super().__init__(master, **kwargs)
+        self._container = Frame(master, borderwidth=2)
+        super().__init__(self._container, **kwargs)
         self._current_position = StringVar()
         self._position_label = Label(master)
-        self._position_label.pack(anchor=NE, side=RIGHT)
         self._position_label.configure(textvariable=self._current_position)
+        self.update_position()
+
+    def pack(self, **kwargs) -> Any:
+        self._container.pack(fill=X)
+        result = super().pack(**kwargs)
+        self._position_label.pack(anchor=NE, side=RIGHT, after=self)
+        return result
 
     def _clicked(self, event=None) -> None:
         super()._clicked(event)
+        self.update_position()
+
+    def update_position(self):
         if self._position_label:
             self._current_position.set(f'{self.position}/{self._to}')
 
@@ -38,3 +49,7 @@ class NavigateSlider(CTkSlider):
         if value > self.position:
             self.position = value
         self.configure(to=value)
+
+    @property
+    def container(self) -> Frame:
+        return self._container
