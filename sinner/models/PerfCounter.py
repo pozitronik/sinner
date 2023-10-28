efbe4871a839099f7a7c7d459ac209ba80365d0e
Pozitronik <antikillerxxl@gmail.com>
2023-10-28 16:43:34 +0400

    issue #18 Some upgrades to model

--- sinner/models/PerfCounter.py
+++ sinner/models/PerfCounter.py
@@ -2,6 +2,10 @@ import time
 
 
 class PerfCounter:
+
+    def __init__(self):
+        self.execution_time: float = 0
+
     def __enter__(self):
         self.start_time = time.perf_counter()
         return self
@@ -9,3 +13,6 @@ class PerfCounter:
     def __exit__(self, *args):
         self.end_time = time.perf_counter()
         self.execution_time = self.end_time - self.start_time
+
+    def __float__(self):
+        return float(self.execution_time)
