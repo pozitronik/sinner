8f6b3c03cbeaa92ecb36ea1181946649af112020
Pozitronik <antikillerxxl@gmail.com>
2023-10-28 18:13:08 +0400

    issue #18 PerfCounter can work in precise mode

--- sinner/models/PerfCounter.py
+++ sinner/models/PerfCounter.py
@@ -3,15 +3,16 @@ import time
 
 class PerfCounter:
 
-    def __init__(self):
+    def __init__(self, ns_mode: bool = False):
         self.execution_time: float = 0
+        self.ns_mode: bool = ns_mode
 
     def __enter__(self):
-        self.start_time = time.perf_counter()
+        self.start_time = time.perf_counter_ns() if self.ns_mode else time.perf_counter()
         return self
 
     def __exit__(self, *args):
-        self.end_time = time.perf_counter()
+        self.end_time = time.perf_counter_ns() if self.ns_mode else time.perf_counter()
         self.execution_time = self.end_time - self.start_time
 
     def __float__(self):
