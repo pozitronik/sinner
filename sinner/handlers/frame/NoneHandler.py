59656ecaa751a276d391a9b2c32294843d2aff27
Pozitronik <antikillerxxl@gmail.com>
2023-10-28 12:42:32 +0400

    issue #18 Implement NoneHandler to handle empty paths

---
+++ sinner/handlers/frame/NoneHandler.py
@@ -0,0 +1,31 @@
+import numpy
+
+from sinner.handlers.frame.BaseFrameHandler import BaseFrameHandler
+from sinner.models.NumberedFrame import NumberedFrame
+from sinner.typing import EmptyFrame
+
+
+# Empty handler for empty targets
+class NoneHandler(BaseFrameHandler):
+
+    # noinspection PyMissingConstructor
+    def __init__(self):
+        pass
+
+    @property
+    def fps(self) -> float:
+        return 0
+
+    @property
+    def fc(self) -> int:
+        return 0
+
+    @property
+    def resolution(self) -> tuple[int, int] | None:
+        return None
+
+    def extract_frame(self, frame_number: int) -> NumberedFrame:
+        return NumberedFrame(0, EmptyFrame)
+
+    def result(self, from_dir: str, filename: str, audio_target: str | None = None) -> bool:
+        return False
