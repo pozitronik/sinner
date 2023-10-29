c3a21a9452dcf4ab5f74a8f312b45ce90a20042b
Pozitronik <antikillerxxl@gmail.com>
2023-10-29 11:06:13 +0400

    issue #18 Null player debug model

---
+++ sinner/gui/controls/NullPlayer.py
@@ -0,0 +1,14 @@
+from sinner.gui.controls.BasePlayer import BasePlayer
+from sinner.typing import Frame
+
+
+class NullPlayer(BasePlayer):
+
+    def __init__(self, **kwargs):
+        pass
+
+    def show_frame(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True) -> None:
+        pass
+
+    def adjust_size(self) -> None:
+        pass
