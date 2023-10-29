46a0024d2051f579666373797462abcb0da78c5a
Pozitronik <antikillerxxl@gmail.com>
2023-10-29 19:54:43 +0400

    issue #18 Implement show_frame_wait() method to show frame for the given time

--- sinner/gui/controls/BasePlayer.py
+++ sinner/gui/controls/BasePlayer.py
@@ -1,5 +1,7 @@
+import time
 from abc import abstractmethod
 
+from sinner.models.PerfCounter import PerfCounter
 from sinner.typing import Frame
 
 
@@ -9,6 +11,19 @@ class BasePlayer:
     @abstractmethod
     def show_frame(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True) -> None:
         pass
+
+    def show_frame_wait(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True, duration: float = 0) -> float:
+        """
+        Shows a frame for the given duration (awaits after frame being shown). If duration is lesser than the frame show time
+        function won't wait
+        :returns await time
+        """
+        with PerfCounter() as timer:
+            self.show_frame(frame=frame, resize=resize)
+        await_time = duration - timer.execution_time
+        if await_time > 0:
+            time.sleep(await_time)
+        return await_time
 
     @abstractmethod
     def adjust_size(self) -> None:
