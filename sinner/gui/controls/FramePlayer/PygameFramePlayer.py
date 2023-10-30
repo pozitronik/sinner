15f91d41eb2820e0a4cec492e6f4221ffdcbf628
Pozitronik <antikillerxxl@gmail.com>
2023-10-30 19:03:45 +0400

    issue #18 Fix the blank screen issue

--- sinner/gui/controls/FramePlayer/PygameFramePlayer.py
+++ sinner/gui/controls/FramePlayer/PygameFramePlayer.py
@@ -10,13 +10,21 @@ from sinner.typing import Frame
 
 class PygameFramePlayer(BaseFramePlayer):
     screen: Surface = None
+    width: int
+    height: int
+    caption: str
 
     def __init__(self, width: int, height: int, caption: str = '😈'):
-        pygame.init()
-        self.screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-        pygame.display.set_caption(caption)
+        self.width = width
+        self.height = height
+        self.caption = caption
 
     def show_frame(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True) -> None:
+        if self.screen is None:
+            # pygame.init()
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+            pygame.display.set_caption(self.caption)
+
         if frame is None and self._last_frame is not None:
             frame = self._last_frame
         if frame is not None:
@@ -26,7 +34,7 @@ class PygameFramePlayer(BaseFramePlayer):
             elif isinstance(resize, tuple):
                 frame = resize_proportionally(frame, resize)
             elif resize is False:  # resize the canvas to the image size
-                self.adjust_size()
+                self.adjust_size(False)
 
         frame = numpy.flip(numpy.rot90((cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))), 0)
         # note: now the frame has the flipped shape (WIDTH, HEIGHT)
@@ -35,7 +43,10 @@ class PygameFramePlayer(BaseFramePlayer):
         self.screen.blit(image_surface, ((self.screen.get_width() - frame.shape[0]) // 2, (self.screen.get_height() - frame.shape[1]) // 2))
         pygame.display.flip()
 
-    def adjust_size(self) -> None:
+    def adjust_size(self, redraw: bool = True) -> None:
         if self._last_frame is not None:
             # note: set_mode size parameter has the WIDTH, HEIGHT dimensions order
             self.screen = pygame.display.set_mode((self._last_frame.shape[1], self._last_frame.shape[0]), pygame.RESIZABLE)
+            # it is required to redraw the frame after resize, if it is not be intended after
+            if redraw:
+                self.show_frame()
