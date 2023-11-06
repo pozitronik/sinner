57bd8505181fcd1026c1bf8623eb8945919eefb0
Pozitronik <antikillerxxl@gmail.com>
2023-11-06 20:35:18 +0400

    issue #18 return type hints

--- sinner/models/PerfCounter.py
+++ sinner/models/PerfCounter.py
@@ -7,13 +7,13 @@ class PerfCounter:
         self.execution_time: float = 0
         self.ns_mode: bool = ns_mode
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         self.start_time = time.perf_counter_ns() if self.ns_mode else time.perf_counter()
         return self
 
-    def __exit__(self, *args):
+    def __exit__(self, *args) -> None:
         self.end_time = time.perf_counter_ns() if self.ns_mode else time.perf_counter()
         self.execution_time = self.end_time - self.start_time
 
-    def __float__(self):
+    def __float__(self) -> float:
         return float(self.execution_time)
