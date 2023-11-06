a6e1840c7a96d4aef5023213ee2eb275c6a2fec0
Pozitronik <antikillerxxl@gmail.com>
2023-11-06 20:02:07 +0400

    issue #18 Update type hints signatures

--- sinner/models/Event.py
+++ sinner/models/Event.py
@@ -3,18 +3,18 @@ from typing import Callable, Any
 
 
 class Event(threading.Event):
-    _on_set_callback: Callable[[Any], Any] | None = None
-    _on_clear_callback: Callable[[Any], Any] | None = None
+    _on_set_callback: Callable[[], Any] | None = None
+    _on_clear_callback: Callable[[], Any] | None = None
 
-    def __init__(self, on_set_callback: Callable[[Any], Any] | None = None, on_clear_callback: Callable[[Any], Any] | None = None):
+    def __init__(self, on_set_callback: Callable[[], Any] | None = None, on_clear_callback: Callable[[], Any] | None = None):
         super().__init__()
         self._on_set_callback = on_set_callback
         self._on_clear_callback = on_clear_callback
 
-    def set_callback(self, callback: Callable[[Any], Any] | None = None) -> None:
+    def set_callback(self, callback: Callable[[], Any] | None = None) -> None:
         self._on_set_callback = callback
 
-    def clear_callback(self, callback: Callable[[Any], Any] | None = None) -> None:
+    def clear_callback(self, callback: Callable[[], Any] | None = None) -> None:
         self._on_clear_callback = callback
 
     def set(self) -> None:
