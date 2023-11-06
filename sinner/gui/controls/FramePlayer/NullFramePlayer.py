7aa1e41e04ecf0c8b2f4394482571175b6920611
Pozitronik <antikillerxxl@gmail.com>
2023-11-06 19:59:52 +0400

    issue #18 Add code stubs to comply the interface

--- sinner/gui/controls/FramePlayer/NullFramePlayer.py
+++ sinner/gui/controls/FramePlayer/NullFramePlayer.py
@@ -4,10 +4,19 @@ from sinner.typing import Frame
 
 class NullFramePlayer(BaseFramePlayer):
 
+    def set_fullscreen(self, fullscreen: bool = True) -> None:
+        pass
+
+    def set_topmost(self, on_top: bool = True) -> None:
+        pass
+
+    def bring_to_front(self) -> None:
+        pass
+
     def __init__(self, **kwargs):
         pass
 
-    def show_frame(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True) -> None:
+    def show_frame(self, frame: Frame | None = None, resize: bool | tuple[int, int] | None = True, rotate: bool = True) -> None:
         pass
 
     def adjust_size(self, redraw: bool = True, size: tuple[int, int] | None = None) -> None:
