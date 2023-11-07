4f2b87dc2a9a81074e7bc799c2ca2bfbbe907cdb
Pozitronik <antikillerxxl@gmail.com>
2023-11-07 15:01:33 +0400

    issue #18 Ignore for now

--- sinner/typing.py
+++ sinner/typing.py
@@ -6,6 +6,6 @@ from insightface.model_zoo.inswapper import INSwapper
 FaceSwapperType = INSwapper
 Frame = numpy.ndarray[Any, Any]
 NumeratedFramePath = tuple[int, str]  # the enumerated path to a frame -> index of the frame and a path to the frame
-FramesList = List[Tuple[Frame, str]]  # list of tuples [frame, frame caption]
+FramesList = List[Tuple[Frame, str]]  # type: ignore[valid-type] # list of tuples [frame, frame caption]
 
 UTF8 = "utf-8"
