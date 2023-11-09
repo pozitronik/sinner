4a9b92933b09ff792695418fa5bd47d04e5e6a30
Pozitronik <antikillerxxl@gmail.com>
2023-11-10 01:01:13 +0400

    issue #65 Code improvements

--- sinner/gui/controls/ThumbnailWidget.py
+++ sinner/gui/controls/ThumbnailWidget.py
@@ -11,37 +11,34 @@ class ThumbnailWidget(Frame):
     thumbnails: List[Tuple[Label, Label]]
     thumbnail_width: int
     thumbnail_height: int
-    canvas: Canvas
+    _columns: int
+    _canvas: Canvas
 
     def __init__(self, master: Misc, **kwargs):  # type: ignore[no-untyped-def]
         super().__init__(master, **kwargs)
         self.thumbnails = []
-        self.thumbnail_width = 100
-        self.thumbnail_height = 100
-        self.columns = 4
-        self.visible_rows = 3
-        self.canvas = Canvas(self)
-        self.canvas.grid(row=0, column=0, sticky=NSEW)
-        self.canvas.grid_rowconfigure(0, weight=1)
-        self.canvas.grid_columnconfigure(0, weight=1)
-        self.frame = Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        self.thumbnail_width = kwargs['thumbnail_width'] if 'thumbnail_width' in kwargs else 200
+        self.thumbnail_height = kwargs['thumbnail_height'] if 'thumbnail_height' in kwargs else 200
+        self._canvas = Canvas(self)
+        self._canvas.grid(row=0, column=0, sticky=NSEW)
+        self._canvas.grid_rowconfigure(0, weight=1)
+        self._canvas.grid_columnconfigure(0, weight=1)
+        self.frame = Frame(self._canvas)
+        self._canvas.create_window((0, 0), window=self.frame, anchor="nw")
 
-        self.vsb = Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.vsb = Scrollbar(self, orient="vertical", command=self._canvas.yview)
         self.vsb.grid(row=0, column=1, sticky=NS)
-        self.canvas.configure(yscrollcommand=self.vsb.set)
+        self._canvas.configure(yscrollcommand=self.vsb.set)
 
-        self.hsb = Scrollbar(self, orient="horizontal", command=self.canvas.xview)
+        self.hsb = Scrollbar(self, orient="horizontal", command=self._canvas.xview)
         self.hsb.grid(row=1, column=0, sticky=EW)
-        self.canvas.configure(xscrollcommand=self.hsb.set)
+        self._canvas.configure(xscrollcommand=self.hsb.set)
 
         self.grid(row=0, column=0, sticky=NSEW)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.canvas.bind("<Configure>", self.on_canvas_resize)
-
-        # Bind the mouse wheel event to scroll the canvas
-        self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
+        self._canvas.bind("<Configure>", self.on_canvas_resize)
+        self._canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
 
     def add_thumbnail(self, image_path: str, caption: str | bool = True, click_callback: Callable[[str], None] | None = None) -> None:
         """
@@ -76,24 +73,24 @@ class ThumbnailWidget(Frame):
     # noinspection PyTypeChecker
     def update_layout(self) -> None:
         total_width = self.winfo_width()
-        self.columns = max(1, total_width // (self.thumbnail_width + 10))
+        self._columns = max(1, total_width // (self.thumbnail_width + 10))
         for i, (thumbnail, caption) in enumerate(self.thumbnails):
-            row = i // self.columns
-            col = i % self.columns
+            row = i // self._columns
+            col = i % self._columns
             thumbnail.grid(row=row * 2, column=col)
             caption.grid(row=row * 2 + 1, column=col, )
 
     # noinspection PyUnusedLocal
     def on_canvas_resize(self, event: Event) -> None:  # type: ignore[type-arg]
-        self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
+        self._canvas.configure(scrollregion=self._canvas.bbox(ALL))
         self.update_layout()
 
     def on_mouse_wheel(self, event: Event) -> None:  # type: ignore[type-arg]
-        self.canvas.yview_scroll(-1 * (event.delta // 120), UNITS)
+        self._canvas.yview_scroll(-1 * (event.delta // 120), UNITS)
 
     def clear_thumbnails(self) -> None:
         for thumbnail, caption in self.thumbnails:
             thumbnail.grid_forget()
             caption.grid_forget()
         self.thumbnails = []
-        self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
+        self._canvas.configure(scrollregion=self._canvas.bbox(ALL))
