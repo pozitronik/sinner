159fc1dbb8d3a6f1ac5fbd5cf98c857045673231
Pozitronik <antikillerxxl@gmail.com>
2024-03-02 12:43:46 +0400

    Add VLCAudioBackend as the test case

--- tests/audio/test_base_audio_backend.py
+++ tests/audio/test_base_audio_backend.py
@@ -5,6 +5,7 @@ import pytest
 from sinner.Parameters import Parameters
 from sinner.models.audio import PygameAudioBackend
 from sinner.models.audio.BaseAudioBackend import BaseAudioBackend
+from sinner.models.audio.VLCAudioBackend import VLCAudioBackend
 from tests.constants import target_mp4
 
 parameters: Namespace = Parameters().parameters
@@ -12,5 +13,6 @@ parameters: Namespace = Parameters().parameters
 
 def test_audio_backend_factory() -> None:
     assert (BaseAudioBackend.create(backend_name='PygameAudioBackend', parameters=parameters, media_path=target_mp4), PygameAudioBackend)
+    assert (BaseAudioBackend.create(backend_name='VLCAudioBackend', parameters=parameters, media_path=target_mp4), VLCAudioBackend)
     with pytest.raises(Exception):
         BaseAudioBackend.create(backend_name='UnknownBackend', parameters=parameters, media_path=target_mp4)
