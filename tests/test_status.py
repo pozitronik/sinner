f3693c28f96da9751907fbab9d344141ab49eeba
Pozitronik <antikillerxxl@gmail.com>
2024-10-08 13:39:58 +0400

    return logger tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,9 +1,13 @@
 from argparse import Namespace
 
+import pytest
 from colorama import Fore, Back
 
 from sinner.Parameters import Parameters
 from sinner.Status import Status, Mood
+from sinner.typing import UTF8
+from sinner.validators.LoaderException import LoadingException
+from tests.constants import test_logfile
 
 
 def test_status(capsys) -> None:
@@ -23,3 +27,19 @@ def test_status_force_emoji(capsys) -> None:
     captured = capsys.readouterr()
     captured = captured.out.strip()
     assert captured.find('😈') != -1
+
+
+def test_status_log() -> None:
+    parameters: Namespace = Parameters(f'--log="{test_logfile}" --enable_emoji=0').parameters
+    status = Status(parameters=parameters)
+
+    status.update_status('test', 'self', Mood.BAD)
+    with open(test_logfile, encoding=UTF8) as file:
+        actual_content = file.read()
+    assert actual_content.find('self: test') != -1
+
+
+def test_status_error() -> None:
+    parameters: Namespace = Parameters(f'--log="/dev/random/incorrect:file\\path*?"').parameters
+    with pytest.raises(LoadingException):
+        assert Status(parameters=parameters)
