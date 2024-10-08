dbafec1ea25b4d02e6561c596c965f285e08b80f
Pozitronik <antikillerxxl@gmail.com>
2024-10-08 10:43:55 +0400

    Unused variable

--- sinner/gui/GUIModel.py
+++ sinner/gui/GUIModel.py
@@ -69,7 +69,6 @@ class GUIModel(Status):
 
     # internal variables
     _is_target_frames_extracted: bool = False
-    _last_requested_frame: int = 0  # the last requested frame inside processing, needed to avoid re-requesting on same frames
     _biggest_processed_frame: int = 0  # the last (by number) processed frame index, needed to indicate if processing gap is too big
     _largest_miss: int = 0  # experimental
     _average_processing_time: MovingAverage = MovingAverage(window_size=10)  # Calculator for the average processing time
