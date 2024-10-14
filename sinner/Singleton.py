cd70debe3c6bb84bd464eb0da26bbf56d9775c78
Pozitronik <antikillerxxl@gmail.com>
2024-10-14 13:29:25 +0400

    type hint

--- sinner/Singleton.py
+++ sinner/Singleton.py
@@ -2,7 +2,7 @@ from typing import Any
 
 
 class Singleton(type):
-    _instances = {}
+    _instances: dict[type, Any] = {}
 
     def __call__(cls, *args: Any, **kwargs: Any) -> Any:
         if cls not in cls._instances:
