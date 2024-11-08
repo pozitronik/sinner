b16ab1b34a8d96958a33c69d93bdaaa44e889ebf
Pozitronik <antikillerxxl@gmail.com>
2024-11-08 14:16:39 +0400

    Show extracted frames on progress

--- sinner/gui/GUIModel.py
+++ sinner/gui/GUIModel.py
@@ -500,6 +500,9 @@ class GUIModel(AttributeLoader, StatusMixin):
             if state_is_finished:
                 self._target_handler = DirectoryHandler(state.path, self.parameters, self.frame_handler.fps, self.frame_handler.fc, self.frame_handler.resolution)
             self._is_target_frames_extracted = state_is_finished
+            if self._ProgressBar:
+                self._ProgressBar.set_segments(self.frame_handler.fc + 1)
+                self._ProgressBar.set_segment_values(state.processed_frames_indices, PROCESSING)
         return self._is_target_frames_extracted
 
     @staticmethod
