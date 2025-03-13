4437ec905fd913831202942890992a2efdfc99ab
Pozitronik <antikillerxxl@gmail.com>
2025-03-13 20:26:37 +0400

    Silence flake8 messages for intentional style violations

--- sin.py
+++ sin.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 import os
-
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  # do not flood with oneDNN spam
-
 import signal
 import sys
 from argparse import Namespace
 
-if sys.version_info < (3, 10):
-    print('Python version is not supported - please upgrade to 3.10 or higher.')
-    quit()
 
-from sinner.Benchmark import Benchmark
-from sinner.Parameters import Parameters
-from sinner.BatchProcessingCore import BatchProcessingCore
-from sinner.Sinner import Sinner
-from sinner.gui.GUIForm import GUIForm
-from sinner.webcam.WebCam import WebCam
-from sinner.utilities import limit_resources
+def init_environment() -> None:
+    """Инициализация переменных окружения перед импортами."""
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  # do not flood with oneDNN spam
+
+    # single thread doubles cuda performance - needs to be set before torch import
+    if any(arg.startswith('--execution-provider') for arg in sys.argv):
+        os.environ['OMP_NUM_THREADS'] = '1'
+
+    if sys.version_info < (3, 10):
+        print('Python version is not supported - please upgrade to 3.10 or higher.')
+        sys.exit(1)
+
+
+init_environment()
+
+from sinner.Benchmark import Benchmark  # noqa: E402
+from sinner.Parameters import Parameters  # noqa: E402
+from sinner.BatchProcessingCore import BatchProcessingCore  # noqa: E402
+from sinner.Sinner import Sinner  # noqa: E402
+from sinner.gui.GUIForm import GUIForm  # noqa: E402
+from sinner.webcam.WebCam import WebCam  # noqa: E402
+from sinner.utilities import limit_resources  # noqa: E402
 
 
 class Sin(Sinner):
@@ -49,9 +58,4 @@ class Sin(Sinner):
 
 
 if __name__ == '__main__':
-    # todo: remnants of roop code, needs to be checked
-    # single thread doubles cuda performance - needs to be set before torch import
-    if any(arg.startswith('--execution-provider') for arg in sys.argv):
-        os.environ['OMP_NUM_THREADS'] = '1'
-
     Sin().run()
